b2d7b2e6e4ef08fd90ab32a7aa92eca8f87a8644
zhangyu18 <zhangyu18@yonyou.com>
2022-03-21 18:47:20 +0800

    RestTemplate请求学习(未完成)

--- springcloud-service-provider-03/src/main/java/com/yonyou/SpringcloudServiceProviderApplication9094.java
+++ springcloud-service-provider-03/src/main/java/com/yonyou/SpringcloudServiceProviderApplication9094.java
@@ -9,10 +9,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class SpringcloudServiceProviderApplication9093 {
+public class SpringcloudServiceProviderApplication9094 {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringcloudServiceProviderApplication9093.class, args);
+        SpringApplication.run(SpringcloudServiceProviderApplication9094.class, args);
     }
 
 }
---
+++ springcloud-service-provider/src/main/java/com/yonyou/entity/UserVo.java
@@ -0,0 +1,29 @@
+package com.yonyou.entity;
+
+/**
+ * @author zhangyu18
+ * @date 2022-03-21 18:10
+ * @since ncc2005
+ */
+public class UserVo {
+
+    /**
+     * 用户主键
+     */
+    protected String cUserId;
+
+    /**
+     * 密码
+     */
+    protected String userPassword;
+
+    /**
+     * 用户名
+     */
+    protected String userName;
+
+    /**
+     * 编码
+     */
+    protected String userCode;
+}
