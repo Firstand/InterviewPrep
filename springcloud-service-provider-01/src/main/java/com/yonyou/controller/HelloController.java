d8a8dcfeb863aca483364b14321eef21d0226c70
zhangyu18 <zhangyu18@yonyou.com>
2022-06-26 13:42:19 +0800

    前端提交

--- springcloud-service-provider-01/src/main/java/com/yonyou/controller/HelloController.java
+++ springcloud-service-provider-01/src/main/java/com/yonyou/controller/HelloController.java
@@ -1,13 +1,87 @@
 package com.yonyou.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.yonyou.entity.UserVo;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * @author zhangyu
+ */
 @RestController
 public class HelloController {
 
     @RequestMapping("hello")
     private String hello() {
         return "你好，我是服务9092";
+    }
+
+    @PostMapping("hello/addUser")
+    private UserVo addUser(@RequestParam("cUserId") String cUserId,
+                           @RequestParam("userPassword") String userPassword,
+                           @RequestParam("userName") String userName,
+                           @RequestParam("userCode") String userCode) {
+        UserVo userVo = new UserVo();
+        userVo.setcUserId(cUserId);
+        userVo.setUserPassword(userPassword);
+        userVo.setUserName(userName);
+        userVo.setUserCode(userCode);
+        userVo.setUserNode("你好，我是服务9092。现在执行新增操作！");
+        System.out.println(userVo);
+        return userVo;
+    }
+
+    @PostMapping("hello/deleteUser")
+    private UserVo deleteUser(@RequestParam("cUserId") String cUserId,
+                           @RequestParam("userPassword") String userPassword,
+                           @RequestParam("userName") String userName,
+                           @RequestParam("userCode") String userCode) {
+        UserVo userVo = new UserVo();
+        userVo.setcUserId(cUserId);
+        userVo.setUserPassword(userPassword);
+        userVo.setUserName(userName);
+        userVo.setUserCode(userCode);
+        userVo.setUserNode("你好，我是服务9092。现在执行删除操作！");
+        System.out.println(userVo);
+        return userVo;
+    }
+
+    @PutMapping("hello/updateUser")
+    public void updateUser(@RequestParam("cUserId") String cUserId,
+                           @RequestParam("userPassword") String userPassword,
+                           @RequestParam("userName") String userName,
+                           @RequestParam("userCode") String userCode) {
+        UserVo userVo = new UserVo();
+        userVo.setcUserId(cUserId);
+        userVo.setUserPassword(userPassword);
+        userVo.setUserName(userName);
+        userVo.setUserCode(userCode);
+        userVo.setUserNode("你好，我是服务9092。现在执行删除操作！");
+        System.out.println(userVo);
+    }
+
+    @GetMapping("hello/queryUser")
+    private UserVo queryUser(@RequestParam("cUserId") String cUserId,
+                              @RequestParam("userPassword") String userPassword,
+                              @RequestParam("userName") String userName,
+                              @RequestParam("userCode") String userCode) {
+        UserVo userVo = new UserVo();
+        userVo.setcUserId(cUserId);
+        userVo.setUserPassword(userPassword);
+        userVo.setUserName(userName);
+        userVo.setUserCode(userCode);
+        userVo.setUserNode("你好，我是服务9092。现在执行查询操作！");
+        System.out.println(userVo);
+        return userVo;
+    }
+
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                sum+=2;
+                System.out.print(i + "_" + j +"("+sum+") ");
+            }
+            System.out.println();
+        }
     }
 }
--- springcloud-service-provider/src/main/java/com/yonyou/entity/UserVo.java
+++ springcloud-service-provider/src/main/java/com/yonyou/entity/UserVo.java
@@ -26,4 +26,49 @@ public class UserVo {
      * 编码
      */
     protected String userCode;
+
+    /**
+     * 用户备注
+     */
+    protected String userNode;
+
+    public String getcUserId() {
+        return cUserId;
+    }
+
+    public void setcUserId(String cUserId) {
+        this.cUserId = cUserId;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getUserNode() {
+        return userNode;
+    }
+
+    public void setUserNode(String userNode) {
+        this.userNode = userNode;
+    }
 }
